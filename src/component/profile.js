80a189da016b57d8177d2b8931ab3c50e1242709
UmutAslan-cloud <umutchinax@gmail.com>
2021-03-24 20:45:43 +0100

    Profile Page

---
+++ src/component/profile.js
@@ -0,0 +1,45 @@
+
+export default function Profil(){
+    return (<div>
+      <header>
+          <p>Merhaba ben Umut Aslan.1995 Istanbul doğumluyum.Ilkokul ve Lise eğitimimi Türkiyede tamamladıktan sonra Çin Halk Cumhuriyetine
+          Üniversite eğitimi için gittim.Wuhan Universitesi Çin Dili ve Edebiyatı Fakültesinde 4 senelik eğitimimi tamamladıktan sonra ticaret hayatına başladım
+          Çeşitli firmaların Çin temsilcisi olarak çalıştım.Sonrasında İsviçreye taşındım ve şuan IT alanında eğitim goruyorum.
+          </p>
+            <table>
+                <tr>
+                    <td>
+                        <b>iSİM</b>
+                    </td>
+                     <td>
+                         Umut
+                     </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Soyisim</b>
+                    </td>
+                     <td>
+                         Aslan
+                     </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Iletişim</b>
+                    </td>
+                     <td>
+                         umutchinax@gmail.com
+                     </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Hobbies</b>
+                    </td>
+                     <td>
+                         Futbol,Film,Kitap
+                     </td>
+                </tr>
+                </table>    
+                </header>
+    </div>)
+}
